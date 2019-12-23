dc35a63bdf84819bdee008992763530275e34f95
Jason Campos <jcampos8782@gmail.com>
2019-12-23 14:59:16 -0800

    Remove unused imports

--- HelloWebApi/Repositories/TodoItemRepository.cs
+++ HelloWebApi/Repositories/TodoItemRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using HelloWebApi.Models;
 using Microsoft.EntityFrameworkCore;
-using MySql.Data.MySqlClient;
 
 namespace HelloWebApi.Repositories
 {
