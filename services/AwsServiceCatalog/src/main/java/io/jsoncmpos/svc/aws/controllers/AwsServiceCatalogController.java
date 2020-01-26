79c57e0017ed81d856b47f341b9184acf31e4206
Jason Campos <jcampos8782@gmail.com>
2020-01-26 13:49:23 -0800

    Basic layout for AWS service catalog

--- services/AwsServiceCatalog/src/main/java/io/jsoncmpos/svc/aws/controllers/AwsServiceCatalogController.java
+++ services/AwsServiceCatalog/src/main/java/io/jsoncmpos/svc/aws/controllers/AwsServiceCatalogController.java
@@ -3,6 +3,7 @@ package io.jsoncmpos.svc.aws.controllers;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,22 +26,26 @@ public class AwsServiceCatalogController {
 		this.catalogSearchSvc = catalogSearchSvc;
 		this.catalogSvc = catalogSvc;
 	}
-	
+
+	@CrossOrigin
 	@GetMapping
 	public Collection<AwsServiceCatalogItem> getCatalogItems() {
 		return catalogSvc.getAll(); 
 	}
-	
+
+	@CrossOrigin
 	@GetMapping("/groups")
 	public Collection<String> getGroups() {
 		return catalogSvc.getGroups();
 	}
-	
+
+	@CrossOrigin
 	@GetMapping("/groups/{groupName}")
 	public Collection<AwsServiceCatalogItem> getCatalogItemsByGroup(@PathVariable("groupName") String group) {
 		return catalogSearchSvc.findCatalogItemsByGroup(group);
 	}
-	
+
+	@CrossOrigin
 	@GetMapping("/{serviceName}")
 	public AwsServiceCatalogItem getCatalogItemByName(@PathVariable("serviceName") String service) {
 		AwsServiceCatalogItem item = catalogSearchSvc.findCatalogItemByName(service);
@@ -51,6 +56,7 @@ public class AwsServiceCatalogController {
 		return item;
 	}
 	
+	@CrossOrigin
 	@GetMapping("/search")
 	public Collection<AwsServiceCatalogItem> getCatalogItems(@RequestParam("q") String query) {
 		return catalogSearchSvc.findCatalogItems(query);
